308bd68d50ed5d34182b7dc0198ac2d3ec188ee9
Parashar365D <parashar365d@gmail.com>
2024-04-30 10:28:12 +0530

    Updates

---
+++ precache-manifest.6436c46c942e9f9130d55c32bd742f46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075a1f6ffb4569d14cd86e3186b72bea",
+    "url": "/ProGames/index.html"
+  },
+  {
+    "revision": "67e3d9c84953382cb6eb",
+    "url": "/ProGames/static/css/main.bc7e8188.chunk.css"
+  },
+  {
+    "revision": "c2cbc90be1eeb7b0e8d2",
+    "url": "/ProGames/static/js/2.9743c2ed.chunk.js"
+  },
+  {
+    "revision": "a6082b8127a0981f31a709f5ad9a2cb4",
+    "url": "/ProGames/static/js/2.9743c2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e032a9bbe223f9f10ee3",
+    "url": "/ProGames/static/js/3.07edf8b6.chunk.js"
+  },
+  {
+    "revision": "67e3d9c84953382cb6eb",
+    "url": "/ProGames/static/js/main.88b42239.chunk.js"
+  },
+  {
+    "revision": "039c4171fe74d00bf784",
+    "url": "/ProGames/static/js/runtime-main.d23f449c.js"
+  }
+]);
